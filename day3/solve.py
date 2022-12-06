afa69e5f85966b5a02a3b2ec0940004700a81421
Srikanth K <karanam.sri@gmail.com>
2022-12-06 23:46:39 +0800

    day 3 solution

--- day3/solve.py
+++ day3/solve.py
@@ -1,47 +1,50 @@
 def main():
     data = parse_file("data.txt")
-    
+
     e_len = len(data[0])
     d_len = len(data)
 
     # Part 1
     gamma = [0] * e_len
-    epsilon = []
+    epsilon = [0] * e_len
     for i in range(e_len):
-        gamma[i] = common_bit(data, i)
-        epsilon.append(int(not gamma[i]))    
+        gamma[i], epsilon[i]  = common_bits(data, i)        
     gamma_int = int(''.join(str(i) for i in gamma), 2)
     epsilon_int = int(''.join(str(i) for i in epsilon), 2)
     print("Answer 1: ", gamma_int * epsilon_int, sep="") # 2261546
 
     # Part 2
     oxy_data = data.copy()
-    i = 0    
-    while len(oxy_data) > 1 and i < e_len:
-        oxy_data = filterby_bitindex(oxy_data, i, str(common_bit(oxy_data, i)))
-        i += 1
-    oxy_int = int(''.join(str(i) for i in oxy_data), 2)
-
     co2_data = data.copy()
-    i = 0    
-    while len(co2_data) > 1 and i < e_len:
-        co2_data = filterby_bitindex(co2_data, i, str(int(not common_bit(co2_data, i))))
-        i += 1
-    co2_int = int(''.join(str(i) for i in co2_data), 2)    
-    
-    print("Answer 2: ", oxy_int * co2_int, sep="") # 
+    for i in range(e_len):
+        most, _ = common_bits(oxy_data, i)
+        _, least = common_bits(co2_data, i)
+        if len(oxy_data) > 1:
+            oxy_data = filterby_bitindex(oxy_data, i, most)
+        if len(co2_data) > 1:
+            co2_data = filterby_bitindex(co2_data, i, least)
 
+    oxy_int = int(''.join(str(i) for i in oxy_data), 2)
+    co2_int = int(''.join(str(i) for i in co2_data), 2)
 
+    print("Answer 2: ", oxy_int * co2_int, sep="") # 6775520
 
-def common_bit(data, index):
-    ones = 0
-    for d in data:
-        if d[index] == '1':
-            ones += 1
-    return 1 if ones > len(data) - ones else 0
+# return counts 1s, 0s
+def count_bits(data, index): 
+    ones = sum([int(d[index]) for d in data])             
+    return ones, len(data) - ones
+
+# return most, least common
+def common_bits(data, index): 
+    ones, zeroes = count_bits(data, index)
+    if ones >= zeroes:
+        return 1, 0  
+    else: 
+        return 0, 1 
 
 def filterby_bitindex(data, index, bit):
-    return [x for x in data if x[index] == bit]    
+    b = str(bit)
+    return [x for x in data if x[index] == b]    
 
 def parse_file(fname):
     with open(fname, "r") as file:
@@ -51,4 +54,5 @@ def parse_file(fname):
             result.append(d.strip())
     return result
 
-main()
+if __name__ == "__main__":
+    main()
